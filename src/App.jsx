54c45446419fd1dcc479dc0a0c074a28afc13c76
小老正 <zizhengmeng@192.168.1.11>
2025-12-30 12:34:28 +0800

    Initial commit: 问候小工具

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState('')
+
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>问候小工具</h1>
+        <input
+          type="text"
+          placeholder="请输入你的名字"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="input"
+        />
+        {name && (
+          <div className="greeting">
+            你好，<span className="name">{name}</span>！
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
